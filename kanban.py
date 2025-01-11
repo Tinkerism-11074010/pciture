bc0a7147b5f6223b779de63281609362e30062a0
Tinkerism-11074010 <57346141+Tinkerism-11074010@users.noreply.github.com>
2025-01-12 00:48:45 +0800

    Update kanban.py

--- kanban.py
+++ kanban.py
@@ -20,7 +20,7 @@ from io import BytesIO
 #         return None
 
 # GitHub 文件 URL
-github_file_url = "https://github.com/Tinkerism-11074010/pciture/blob/main/streamlit专用-阉割版.xlsx"
+github_file_url = "https://raw.githubusercontent.com/Tinkerism-11074010/pciture/blob/main/streamlit专用-阉割版.xlsx"
 
 # 发送 GET 请求
 response = requests.get(github_file_url)
